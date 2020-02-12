efa7229326d914cafb6de78ee39d472d1ea1556f
salwa31 <38915161+salwa31@users.noreply.github.com>
2020-02-11 20:48:10 -0500

    [NOR-19] - named branch correctly and made all the necessary changes

---
+++ server/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -0,0 +1,18 @@
+package ca.utoronto.utm.mcs;
+
+import com.mongodb.*;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import com.mongodb.client.MongoCollection;
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+        //Establishing a connection to a Mlabs cluster where so far it stores the restaurants and their hours of operations
+        String building = "Davis";
+        MongoDBConnector connector = new MongoDBConnector();
+        MongoClient connection = connector.getMongoDBConnection();
+        RetrieveHoursOfOperation result = new RetrieveHoursOfOperation(connection, building);
+    }
+}
