bfc188a3f0cee77151db69021db7957d3179a0ee
royyu <royyu1997@gmail.com>
2020-02-12 23:10:58 -0500

    implement basic login template

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  images: string[];
+  email: string = '';
+
+  constructor() {
+    this.images = [
+      'lily-banse--YHSwy6uqvk-unsplash.jpg',
+      'abhishek-sanwa-limbu-LR559Dcst70-unsplash.jpg',
+      'eiliv-sonas-aceron-ZuIDLSz3XLg-unsplash.jpg',
+      'brooke-lark-M4E7X3z80PQ-unsplash.jpg',
+      'brooke-lark-W9OKrxBqiZA-unsplash.jpg',
+      'edgar-castrejon-1SPu0KT-Ejg-unsplash.jpg',
+      'brenda-godinez-_Zn_7FzoL1w-unsplash.jpg',
+      'monika-grabkowska-P1aohbiT-EY-unsplash.jpg',
+      'brooke-lark-HlNcigvUi4Q-unsplash.jpg'
+    ];
+  }
 
   ngOnInit() {
   }
