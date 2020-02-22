2f25db60429a8cee27a7b24f633839a9af302126
henduMain <ken.cui@mail.utoronto.ca>
2020-02-21 22:04:29 -0500

    Update Txt_information_spliter.java

--- python menu scrapper/Txt_information_spliter.java
+++ python menu scrapper/Txt_information_spliter.java
@@ -17,8 +17,8 @@ import com.sun.net.httpserver.HttpHandler;
 public class Txt_information_spliter {
     private String Restaurant;
     private String File;
-
-    
+    private JSONObject response = new JSONObject(new LinkedHashMap());
+    private JSONArray Type = new JSONArray();
     
     public Txt_information_spliter(String Restaurant_name,String File_name) {
    
@@ -32,16 +32,20 @@ public class Txt_information_spliter {
         
         try {
 			BufferedReader in = new BufferedReader(new FileReader(File));
-			System.out.println("File Found");
-			System.out.println("ame:"+Restaurant);
+//			System.out.println("File Found");
+//			System.out.println("ame:"+Restaurant);
 			while((line = in.readLine()) != null){
-				System.out.println(line);
-				if (line.contains("name:"+Restaurant)){
-					System.out.println(line);
+//				System.out.println(line);
+				if (line.contains("ame:"+Restaurant)){
 					line = in.readLine();
-					System.out.println(line);
+					get_tag(line);
+					System.out.println("tag: "+get_tag(line));
 					line = in.readLine();
-					System.out.println(line);
+					String[] dishes = get_dishes(line);
+					for (String a : dishes) {
+			            System.out.println(a); 
+					}
+
 				}
 //		        String[] var = line.split(":");
 		    }
@@ -57,13 +61,25 @@ public class Txt_information_spliter {
 
     	return null;
     }
-    public String get_menu() {
-    	
+    public String get_tag(String line) {
+    	if (line.contains(":")) {
+    		String[] v = line.split(":");
+    		return v[0];
+    	}
 		return null;
     	
     }
+
+    public String[] get_dishes(String line) {
+    	if (line.contains(";")) {
+    		String[] v = line.replace(";", "").split(",");
+    		return v;
+    	}
+		return null;	
+    }
     
-    public String get_price() {
+    public String get_price(String line) {
+    	
     	return null;
     }
     
