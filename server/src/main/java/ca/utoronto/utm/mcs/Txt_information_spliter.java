f3b95f6220e076c355da40f96e620f218119260c
henduMain <ken.cui@mail.utoronto.ca>
2020-02-26 02:41:07 -0500

    Now the jason would have Nutritional and Calories

--- server/src/main/java/ca/utoronto/utm/mcs/Txt_information_spliter.java
+++ server/src/main/java/ca/utoronto/utm/mcs/Txt_information_spliter.java
@@ -13,7 +13,7 @@ import org.json.*;
 public class Txt_information_spliter {
     private String Restaurant;
     private String File;
-
+    private String[] Nutritional ;
     private String line;
     
     public Txt_information_spliter(String Restaurant_name,String File_name) {
@@ -30,6 +30,7 @@ public class Txt_information_spliter {
 			BufferedReader in = new BufferedReader(new FileReader(File));
 			while((line = in.readLine()) != null){
 				if (line.contains("ame:"+Restaurant)){
+					get_Nutritional();
 //					System.out.println(detect_line_type(in));
 					result = detect_line_type(in);
 				}
@@ -46,12 +47,26 @@ public class Txt_information_spliter {
 
     	return result;
     }
+    public String[] get_Nutritional() {
+    	if (line.contains("ame:")){
+    		if (line.contains("+")) {
+        		String Nus = (line.split("@")[1]);
+        		Nutritional = Nus.split("+");
+    		}
+    		else {
+    			Nutritional = new String[] {line.split("@")[1]};
+
+    		}
+		}
+		return null;
+    }
     public JSONObject get_tag(String line) {
     	if (line.contains(":")) {
     		String[] v = line.split(":");
     		JSONObject Tag_body = new JSONObject();
     		try {
 				Tag_body.put("tag",v[0].replaceAll("_", " "));
+
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -74,10 +89,16 @@ public class Txt_information_spliter {
     public JSONObject detect_line_type(BufferedReader in) {
     	JSONObject tag = null;
     	JSONObject whole = new JSONObject();
+		try {
+			whole.put("Nutritional", Nutritional);
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
     	try {
 			while((line = in.readLine()) != null){
 				if (line.contains("ame:")){
-					whole.append("Menu",tag);
+					whole.append(Restaurant,tag);
 //					System.out.println("tag ended");
 //					System.out.println(line+" end met"); 
 					return whole;
@@ -86,7 +107,7 @@ public class Txt_information_spliter {
 					//tag detected
 					if (tag != null) {
 //						System.out.println("tag detected, finished scan tag"+tag);
-						whole.append("Menu",tag);
+						whole.append(Restaurant,tag);
 					}
 					tag = get_tag(line);
 				}
@@ -96,14 +117,17 @@ public class Txt_information_spliter {
 					for (String a : dishes) {
 						JSONObject dish = new JSONObject();
 			            dish.put("Name", a.split("=")[0].replaceAll("_", " "));
-			            dish.put("Price", a.split("=")[1].replaceAll("_", " "));
+			            String price_cal =( a.split("=")[1].replaceAll("_", " "));
+//			            System.out.print(price_cal);
+			            dish.put("Price",price_cal.split("#")[0]);
+			            dish.put("Calories",price_cal.split("#")[1]);
 //			            System.out.println(dish); 
 //			            System.out.println(tag); 
 			            tag.append("dishes", dish);
 					}
 				}
 			}
-	    	whole.append("Menu",tag);
+	    	whole.append(Restaurant,tag);
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
