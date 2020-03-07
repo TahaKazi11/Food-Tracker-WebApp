52b889cfa90ab27ebee962c034a8389aa516cb38
xiaxiedaima <897668650@qq.com>
2020-03-07 18:25:54 -0500

    add login and encrp functions
    add login feature and fix put user functions

--- server/src/main/java/ca/utoronto/utm/mcs/PutUser.java
+++ server/src/main/java/ca/utoronto/utm/mcs/PutUser.java
@@ -1,4 +1,5 @@
 package ca.utoronto.utm.mcs;
+import ca.utoronto.utm.mcs.Utility.Utils;
 
 
 import java.util.Iterator;
@@ -21,47 +22,66 @@ import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.bson.BsonArray;
 
-public class PutUser{
+public class PutUser implements HttpHandler{
+    private MongoClient mongoClient;
+    public PutUser(MongoClient client) {
+        this.mongoClient = client;
+    }
 
-    public void handle(HttpExchange r) throws IOException, JSONException {
 
-        MongoClientURI uri = new MongoClientURI("mongodb://north_building:utmfoodtracker@utmfood-shard-00-00-cyzxf.azure.mongodb.net:27017,utmfood-shard-00-01-cyzxf.azure.mongodb.net:27017,utmfood-shard-00-02-cyzxf.azure.mongodb.net:27017/test?ssl=true&replicaSet=UTMFood-shard-0&authSource=admin&retryWrites=true&w=majority&maxIdleTimeMS=5000");
-        MongoClient mongoClient = new MongoClient(uri);
+    public void handle(HttpExchange r) throws IOException{
+        try {
+            if (r.getRequestMethod().equals("PUT")) {
+                this.handlePut(r);
+            }
+            else {
+                System.out.println("a");
+                Utils.writeResponse(r, "", 400);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        String DatabaseName = ""; //to be filled
-        String collectionName = ""; // to be filled
+        private void handlePut(HttpExchange r) throws JSONException, IOException{
+    
+            String DatabaseName = "UTMFoodTracker"; //to be filled
+            String collectionName = "Users"; // to be filled
+    
+            
+            MongoDatabase dbdata = this.mongoClient.getDatabase(DatabaseName); 
+            MongoCollection collection = dbdata.getCollection(collectionName);
+    
+            String body = Utils.convert(r.getRequestBody());
+            JSONObject deserialized = new JSONObject(body);
+    
+            String result = "";
+            try{
+                if (deserialized.has("name")&&deserialized.has("email")&&deserialized.has("password")){
+                        //the user information json format need to be determined
+                        Document userInfo = Document.parse( body );
+                        String encrString = Utils.passEncrypt(userInfo.get("password").toString());
+                        userInfo.put("password", encrString);
+                        collection.insertOne(userInfo);
+                        ObjectId id = (ObjectId)userInfo.get( "_id" );
+                        System.out.println(id);
+                        r.sendResponseHeaders(200, id.toString().getBytes().length);
+                        OutputStream os = r.getResponseBody();
+                        os.write(id.toString().getBytes());
+                        os.close();
+                        }
+
+                else{
+                    System.out.println("b");
+                    r.sendResponseHeaders(400, -1);
+                    return;
+                }
+            }catch (Exception e){
+                r.sendResponseHeaders(500, -1);
+                return;
+                }
+            }
 
         
-        MongoDatabase dbdata = mongoClient.getDatabase(DatabaseName); 
-        MongoCollection collection = dbdata.getCollection(collectionName);
-
-        String body = Utils.convert(r.getRequestBody());
-        JSONObject deserialized = new JSONObject(body);
-
-        String result = "";
-        try{
-            if (r.getRequestBody().equals("PUT")){
-
-                if (deserialized.has("name")){
-                    //the user information json format need to be determined
-                    Document userInfo = Document.parse( body );
-                    collection.insertOne(userInfo);
-                    ObjectId id = (ObjectId)userInfo.get( "_id" );
-                    System.out.println(id);
-                    r.sendResponseHeaders(200, id.toString().getBytes().length);
-                    OutputStream os = r.getResponseBody();
-                    os.write(id.toString().getBytes());
-                    os.close();
-                    }
-                }
-            else{
-                r.sendResponseHeaders(400, -1);
-                return;
-            }
-        }catch (Exception e){
-            r.sendResponseHeaders(500, -1);
-            return;
-            }
-        }
 
     }
--- server/src/main/java/ca/utoronto/utm/mcs/Utility/Utils.java
+++ server/src/main/java/ca/utoronto/utm/mcs/Utility/Utils.java
@@ -5,6 +5,9 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Utils {
     public static void writeResponse(HttpExchange httpExchange, String response, int code) throws IOException
@@ -37,6 +40,34 @@ public class Utils {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
             return br.lines().collect(Collectors.joining(System.lineSeparator()));
         }
+    }
+
+    public static String passEncrypt(String str){
+        MessageDigest messageDigest;
+        String encrptstr = "";
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(str.getBytes("UTF-8"));
+            encrptstr = bytesToHex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encrptstr;
+    }
+
+    private static String bytesToHex(byte[] bytes){
+        StringBuffer stringBuffer = new StringBuffer();
+        String temp = null;
+        for (int i=0;i<bytes.length;i++){
+            temp = Integer.toHexString(bytes[i] & 0xFF);
+            if (temp.length()==1){
+                stringBuffer.append("0");
+            }
+            stringBuffer.append(temp);
+        }
+        return stringBuffer.toString();
     }
 
 }
